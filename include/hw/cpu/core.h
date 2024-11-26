6dcee5f9e6c076631e5566a2d7ce63310758eaf4
Alireza Sanaee <alireza.sanaee@huawei.com>
2024-11-26 17:56:36 +0000

    arm/virt.c: add cache hierarchy to device tree
    Specify which layer (core/cluster/socket) caches found at in the CPU
    topology. Updating cache topology to device tree (spec v0.4).
    Example:
    
    Here, 2 sockets (packages), and 2 clusters, 4 cores and 2 threads
    created, in aggregate 2*2*4*2 logical cores. In the smp-cache object,
    cores will have l1d and l1i.  However, extending this is not difficult).
    The clusters will share a unified l2 level cache, and finally sockets
    will share l3. In this patch, threads will share l1 caches by default,
    but this can be adjusted if case required.
    
    Currently only three levels of caches are supported.  The patch does not
    allow partial declaration of caches. In another word, all caches must be
    defined or caches must be skipped.
    
    ./qemu-system-aarch64 \
        -machine virt,\
             smp-cache.0.cache=l1i,smp-cache.0.topology=core,\
             smp-cache.1.cache=l1d,smp-cache.1.topology=core,\
             smp-cache.2.cache=l2,smp-cache.2.topology=cluster,\
             smp-cache.3.cache=l3,smp-cache.3.topology=socket\
        -cpu max \
        -m 2048 \
        -smp sockets=2,clusters=2,cores=4,threads=1 \
        -kernel ./Image.gz \
        -append "console=ttyAMA0 root=/dev/ram rdinit=/init acpi=force" \
        -initrd rootfs.cpio.gz \
        -bios ./edk2-aarch64-code.fd \
        -nographic
    
    For instance, following device tree will be generated for a scenario
    where we have 2 sockets, 2 clusters, 2 cores and 2 threads, in total 16
    PEs. L1i and L1d are private to each thread, and l2 and l3 are shared at
    socket level as an example.
    
    Limitation: SMT cores cannot share L1 cache for now. This
    problem does not exist in PPTT tables.
    
    Signed-off-by: Alireza Sanaee <alireza.sanaee@huawei.com>
    Co-developed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Signed-off-by: Jonathan Cameron <jonathan.cameron@huawei.com>

--- include/hw/cpu/core.h
+++ include/hw/cpu/core.h
@@ -25,6 +25,32 @@ struct CPUCore {
     int nr_threads;
 };
 
+typedef enum CPUCacheType {
+    DATA,
+    INSTRUCTION,
+    UNIFIED,
+} PPTTCPUCacheType;
+
+typedef struct PPTTCPUCaches {
+    PPTTCPUCacheType type;
+    uint32_t pptt_id;
+    uint32_t sets;
+    uint32_t size;
+    uint32_t level;
+    uint16_t linesize;
+    uint8_t attributes; /* write policy: 0x0 write back, 0x1 write through */
+    uint8_t associativity;
+} PPTTCPUCaches;
+
+int partial_cache_description(const MachineState *ms, PPTTCPUCaches *caches,
+                              int num_caches);
+
+bool cache_described_at(const MachineState *ms, CpuTopologyLevel level);
+
+bool find_the_lowest_level_cache_defined_at_level(const MachineState *ms,
+                                                  int *level_found,
+                                                  CpuTopologyLevel topo_level);
+
 /* Note: topology field names need to be kept in sync with
  * 'CpuInstanceProperties' */
 
