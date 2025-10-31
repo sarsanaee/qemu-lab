9390b35422e51ef1a083387798881474b5f187d1
Alireza Sanaee <alireza.sanaee@huawei.com>
2025-10-31 16:26:05 +0000

    hw/cxl: Change Extent add/remove APIs for lazy memory backend.
    Add extra information in each extent about fix memory window and
    memory backend, because extent might be backed by different memory
    backends, thus such information must be stored in the extent object.
    Consequently, APIs should be changes to support extra members.
    
    Signed-off-by: Alireza Sanaee <alireza.sanaee@huawei.com>

--- hw/cxl/cxl-host.c
+++ hw/cxl/cxl-host.c
@@ -365,6 +365,10 @@ static int cxl_fmws_direct_passthrough(Object *obj, void *opaque)
         return 0;
     }
 
+    if (!ct3d->dc.fw) {
+        ct3d->dc.fw = fw;
+    }
+
     if (state->commit) {
         MemoryRegion *mr = NULL;
         uint64_t vmr_size = 0, pmr_size = 0, dc_mr_size = 0;
